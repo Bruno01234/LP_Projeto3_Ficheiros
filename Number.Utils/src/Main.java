32d473a938ba5c3c1917c4bae9d9eb7a1ae3f442
Bruno01234 <brunolopesjardim@ua.pt>
2024-05-19 17:04:47 +0100

    main - 19/05 17:04

--- Number.Utils/src/Main.java
+++ Number.Utils/src/Main.java
@@ -1,73 +1,41 @@
 import java.util.ArrayList;
-import java.lang.Math;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import Number.Utils;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Double> lista = new ArrayList<>();
+        lista.add(3.0);
+        lista.add(3.0);
+        lista.add(7.0);
+        lista.add(8.0);
+        lista.add(9.0);
+        lista.add(1.0);
+        lista.add(9.0);
+        lista.add(5.0);
+        lista.add(6.0);
+        lista.add(4.0);
+        lista.add(4.0);
+        lista.add(3.0);
+
+        double media = Utils.CalcularMedia(lista);
+        System.out.println("A média é: " + media);
+
+        double mediana = Utils.CalcularMediana(lista);
+        System.out.println("A mediana é " + mediana);
+
+        ArrayList<Double> modas = Utils.CalcularModa(lista);
+        for (Double moda : modas) {
+            System.out.println("A moda é " + moda);
+        }
+
+        System.out.println("A lista em ordem crescente é ");
+        ArrayList<Double> crescente = Utils.OrdenarLista(lista,true);
+        System.out.println(crescente);
+
+        System.out.println("A lista em ordem decrescente é ");
+        ArrayList<Double> decrescente =  Utils.OrdenarLista(lista,false);
+        System.out.println(decrescente);
     }
-
-    double CalcularMedia(ArrayList<Double> numeros) {
-        double soma = 0.0;
-        for (int i = 0; i < numeros.size(); i++) {
-            soma += numeros.get(i);
-        }
-        return soma / numeros.size();
-    }
-
-    double CalcularMediana(ArrayList<Double> numeros) {
-        boolean even;
-        int median = numeros.size() / 2;
-        ArrayList<Double> ordenados = new ArrayList(numeros);
-        Collections.sort(ordenados);
-
-        if (numeros.size() % 2 == 0) {
-            even = false;
-        } else {
-            even = true;
-        }
-
-        if (!even) {
-            return median;
-        } else {
-            return (ordenados.get(median - 1) + ordenados.get(median) / 2.0);
-        }
-
-    }
-
-    ArrayList<Double> CalcularModa(ArrayList<Double> numeros) {
-
-        Map<Double,Integer> frequencia = new HashMap<Double,Integer>();
-        for (double numero : numeros) {
-            frequencia.put(numero,frequencia.getOrDefault(numero,0) + 1);
-        }
-
-        int MaxFrequencia = Collections.max(frequencia.values());
-        ArrayList<Double> modas = new ArrayList<>();
-
-        for (Map.Entry<Double,Integer> entry : frequencia.entrySet()) {
-            if (entry.getValue() == MaxFrequencia) {
-                modas.add(entry.getKey());
-            }
-        }
-        return modas;
-    }
-
-    ArrayList<Double> OrdenarLista(ArrayList<Double> numeros, boolean crescente) {
-
-        if (crescente) {
-            Collections.sort(numeros);
-        } else {
-            Collections.sort(numeros, Collections.reverseOrder());
-        }
-
-        return numeros;
-
-    }
-
 }
-
-
-
---
+++ Number.Utils/src/Number/Utils.java
@@ -0,0 +1,74 @@
+package Number;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Utils {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+  public static double CalcularMedia(ArrayList<Double> numeros) {
+        double soma = 0.0;
+        for (int i = 0; i < numeros.size(); i++) {
+            soma += numeros.get(i);
+        }
+        return soma / numeros.size();
+    }
+
+  public static double CalcularMediana(ArrayList<Double> numeros) {
+        boolean even;
+        int median = numeros.size() / 2;
+        ArrayList<Double> ordenados = new ArrayList(numeros);
+        Collections.sort(ordenados);
+
+        if (numeros.size() % 2 == 0) {
+            even = false;
+        } else {
+            even = true;
+        }
+
+        if (!even) {
+            return median;
+        } else {
+            return (ordenados.get(median - 1) + ordenados.get(median) / 2.0);
+        }
+
+    }
+
+    public static ArrayList<Double> CalcularModa(ArrayList<Double> numeros) {
+
+        Map<Double,Integer> frequencia = new HashMap<Double,Integer>();
+        for (double numero : numeros) {
+            frequencia.put(numero,frequencia.getOrDefault(numero,0) + 1);
+        }
+
+        int MaxFrequencia = Collections.max(frequencia.values());
+        ArrayList<Double> modas = new ArrayList<>();
+
+        for (Map.Entry<Double,Integer> entry : frequencia.entrySet()) {
+            if (entry.getValue() == MaxFrequencia) {
+                modas.add(entry.getKey());
+            }
+        }
+        return modas;
+    }
+
+    public static ArrayList<Double> OrdenarLista(ArrayList<Double> numeros, boolean crescente) {
+
+        if (crescente) {
+            Collections.sort(numeros);
+        } else {
+            Collections.sort(numeros, Collections.reverseOrder());
+        }
+
+        return numeros;
+
+    }
+
+}
+
+
+
